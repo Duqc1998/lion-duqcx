f60c49e3d2c1067d27bf530fd9c1ab95a19e1842
Duqc1998 <1022832160@qq.com>
2021-07-15 00:56:54 +0800

    测试提交

--- src/test/java/org/example/ParentTest.java
+++ src/test/java/org/example/ParentTest.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple Parent.
  */
-public class AppTest 
+public class ParentTest
 {
     /**
      * Rigorous Test :-)
