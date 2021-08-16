c0d9ff8d03a3f951290738226a9909956d1173ad
Duqc1998 <1022832160@qq.com>
2021-08-16 23:40:15 +0800

    修改内容

---
+++ DuqcX_process/src/main/java/org/example/Lambda/OutstandingClass.java
@@ -0,0 +1,13 @@
+package org.example.Lambda;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class OutstandingClass {
+
+    private String className;
+
+    private List<Student> students;
+}
